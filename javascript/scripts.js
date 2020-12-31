4232567c732ae883973eb16f191ed2ef8f5dda1f
Alyssa Song <43687923+alyssa-song@users.noreply.github.com>
2020-12-31 01:53:00 -0500

    Redesigned layout

---
+++ javascript/scripts.js
@@ -0,0 +1,19 @@
+/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content 
+- This allows the user to have multiple dropdowns without any conflict */
+$(".dropdown-btn").click( function(){
+    var dropdown = $(".contact-dropdown");
+    if (dropdown.css("display") === "block") {
+        dropdown.css("display","none");
+        $(".dropdown-btn").html('Contact<i class="fa fa-caret-left"></i>');
+    } else {
+        dropdown.css("display","block");
+        $(".dropdown-btn").html('Contact<i class="fa fa-caret-down"></i>');
+    }
+});
+
+$(document).on('click', 'a[href^="#"]', function (e) {
+    e.preventDefault();
+    $('html, body').stop().animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 500, 'linear');
+});
